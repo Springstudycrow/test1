c2ebc9bb240808678b470a142e1916908c0a7fb0
Crow <oneso123456789@gmail.com>
2022-11-22 14:11:10 +0900

    Test

---
+++ src/main/java/com/example/gittestpj/GitTestPjApplication.java
@@ -0,0 +1,13 @@
+package com.example.gittestpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestPjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTestPjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gittestpj/GitTestPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittestpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestPjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
